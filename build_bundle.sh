4f3e23986e4fa28d0b1e05f5c7af22359380cbdd
David Faure <faure@kde.org>
2026-06-23 20:12:30 +0200

    build script

---
+++ build_bundle.sh
@@ -0,0 +1,1 @@
+cargo build --lib --no-default-features --release --features slint/backend-android-activity-06 && cp target/release/*.so app/src/main/jniLibs/arm64-v8a/ && aarch64-linux-android-strip -s ./app/src/main/jniLibs/arm64-v8a/libtunecall.so && ./gradlew bundleRelease && cp app/build/outputs/bundle/release/*.aab /sdcard/Download/
